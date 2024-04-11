4781dd74fc5c6b56c78dab90e6739d3617bf6953
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-11 19:11:14 -0300

    refactor: improved class structure

--- strategy/employee_tax_problem/employee.py
+++ strategy/employee_tax_problem/employee.py
@@ -1,6 +1,34 @@
-class Employee:
-    def __init__(self, strategy):
-        self.strategy = strategy
+"""
+Essa classe é o "contexto" do padrão Strategy, isto é, ela é responsável por receber e executar o algoritmo selecionado em tempo de execução.
+"""
 
-    def calculate_tax(self, salary):
-        return self.strategy.calculate_tax(salary)
+from tax_strategy import TaxStrategy
+
+
+class Employee:
+    """
+    Classe Contexto, ela é responsável por receber e executar o algoritmo selecionado em tempo de execução.
+    """
+
+    def __init__(self):
+        self.__strategy: TaxStrategy = TaxStrategy()
+        self.__salary: float = 0.0
+
+    @property
+    def salary(self) -> float:
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary: float) -> None:
+        self.__salary = salary
+
+    @property
+    def strategy(self) -> TaxStrategy:
+        return self.__strategy
+
+    @strategy.setter
+    def strategy(self, strategy: TaxStrategy) -> None:
+        self.__strategy = strategy
+
+    def salary_with_tax(self) -> float:
+        return self.strategy.calculate_tax(self.salary)
